eef483d37bdda627dca93ce91992fa89ed8834da
osi001 <85637672+osi001@users.noreply.github.com>
2023-09-15 00:00:45 +0100

    initial+ complete scraper

--- main_crawler.py
+++ main_crawler.py
@@ -1,5 +1,6 @@
 import requests as req
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 # response = req.get("https://uk.investing.com/")
@@ -10,26 +11,55 @@ if response.status_code == 200:
     
     soup = BeautifulSoup(response.text, 'html.parser')
     
+    soup = BeautifulSoup(response.text, 'lxml')
     
-    # all_para = soup.find_all('p')
+    table = soup.find("table", class_ = "genTbl closedTbl ecoCalTbl persistArea js-economic-table")
     
-    # all_links = soup.find_all('a')
+    headers = table.find_all("thead", class_="floatingHeader")
+    # print(headers)
     
-    # for link in all_links:
-    #     print(link)
-    
-    
-    
-    for section in soup.find_all("table", {'id': 'economicCalendarData'}):
-        print (section.find("tbody").text)
+    titles = []
+    for i in headers:
+        
+        headings = i.find_all("th")
+        title = [th.text for th in headings]
+        titles.append(title)
+    # print(titles)
+
+        
+    df = pd.DataFrame(columns=titles)
+    # print(df)
+            
+    rows = table.find_all("tr")
+    # print(rows)
+
+    for i in rows [3:]:
+        data = i.find_all("td")
+        # print(data)
+        row = [tr.text for tr in data]
+        # print(row)
+        l = len(df)
+        df.loc[l] = row
         
         
-        
-    # for section in soup.find_all("table", {'id': 'economicCalendarData'}):
-    #     if (section.find("tbody").text) == "Unemployment Rate":
-    #         print (section)
-        
-    # calenderData = soup.find('table', {'id': 'economicCalendarData'})
-    # print(calenderData)
-        
+       
+    df_cleaned = df.applymap(lambda x: x.replace('\n', ''))    
+   
+    
+    print(df_cleaned)
+    df_cleaned.to_csv("economic_calender_data6.csv")
+    
+    
+    
+    #to save to sql
+    # db_connection = create_engine('sqlite:///my_database.db')  # Replace with your database URL
+    # # Write the DataFrame to a table in the database
+    # table_name = 'my_table'
+    # df.to_sql(table_name, con=db_connection, if_exists='replace', index=False)
+
+    
+    
+    
+    
+    
 
